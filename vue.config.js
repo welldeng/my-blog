c968acc6f5d8870e9405f9276ad54f969d9eab86
Mr-onions <mr-onions@Mr-onionsdeMacBook-Pro.local>
2019-08-22 23:05:11 +0800

    deploy ghpage

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-manage/' : '/',
     configureWebpack: {
-        plugins: [
-        ]
+        plugins: []
     },
     chainWebpack: config => {
         config.module
