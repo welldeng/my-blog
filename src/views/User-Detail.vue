757024bcc11aa88b9a5a2b6e3544f8bc6c3c7668
Mr-onions <mr-onions@Mr-onionsdeMacBook-Pro.local>
2019-08-22 22:31:03 +0800

    perf:个人主页完成

--- src/views/User-Detail.vue
+++ src/views/User-Detail.vue
@@ -5,36 +5,95 @@
                  left-arrow
                  @click-left="goBack">
         </nav-bar>
-        <div class="user-info">
-            <h3>基本资料</h3>
+        <div class="user-contain">
             <div class="user-info">
-                <div class="user-info-avatar">
-                    <img src="../assets/img/pikaqiu3.jpg" alt="头像图片">
-                </div>
-                <div class="user-info-detail">
-                    <div>
-                        <div class="user-info-detail-title">姓名</div>
-                        <div class="user-info-detail-content">邓邓邓</div>
+                <h3>基本资料</h3>
+                <div class="user-info-box">
+                    <div class="user-info-avatar">
+                        <img src="../assets/img/pikaqiu3.jpg" alt="头像图片">
                     </div>
-                    <div>
-                        <div class="user-info-detail-title">年龄</div>
-                        <div class="user-info-detail-content">26</div>
-                    </div>
-                    <div>
-                        <div class="user-info-detail-title">毕业学校</div>
-                        <div class="user-info-detail-content">广东工业大学</div>
-                    </div>
-                    <div>
-                        <div class="user-info-detail-title">个人主页</div>
-                        <div class="user-info-detail-content">
-                            <a href="https://juejin.im/user/5b35a754e51d4558ac48a299" target="_blank">https://juejin.im/user/5b35a754e51d4558ac48a299</a>
+                    <div class="user-info-detail">
+                        <div>
+                            <div class="user-info-detail-title">姓名</div>
+                            <div class="user-info-detail-content">邓邓邓</div>
+                        </div>
+                        <div>
+                            <div class="user-info-detail-title">年龄</div>
+                            <div class="user-info-detail-content">26</div>
+                        </div>
+                        <div>
+                            <div class="user-info-detail-title">毕业学校</div>
+                            <div class="user-info-detail-content">广东**大学</div>
+                        </div>
+                        <div>
+                            <div class="user-info-detail-title">个人主页</div>
+                            <div class="user-info-detail-content">
+                                <a href="https://juejin.im/user/5b35a754e51d4558ac48a299" target="_blank">https://juejin.im/user/5b35a754e51d4558ac48a299</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="user-experience">
-            <h3>工作</h3>
+            <div class="user-experience">
+                <h3>工作经验</h3>
+                <ul class="user-experience-ul">
+                    <li>
+                        <div class="work-time">
+                            <span>xx公司</span>
+                            <span>2018/05-2019/08</span>
+                        </div>
+                        <div class="work-duty">
+                            前端开发工程师
+                        </div>
+                        <div class="work-description">
+                            <div>工作描述：</div>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                        </div>
+                        <div class="project-description">
+                            <h4>xx项目</h4>
+                            <h4>项目描述：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                            <h4>项目职责：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="work-time">
+                            <span>xx公司</span>
+                            <span>2016/07-2018/05</span>
+                        </div>
+                        <div class="work-duty">
+                            前端开发工程师
+                        </div>
+                        <div class="work-description">
+                            <div>工作描述：</div>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                        </div>
+                        <div class="project-description">
+                            <h4>xx项目</h4>
+                            <h4>项目描述：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                            <h4>项目职责：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                        </div>
+                        <div class="project-description">
+                            <h4>xx项目</h4>
+                            <h4>项目描述：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                            <h4>项目职责：</h4>
+                            <p>
+                                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -51,22 +110,33 @@
 </script>
 
 <style scoped lang="less">
+    .user-contain {
+        background: url('~@/assets/img/pkm-bg2.jpg') fixed;
+        background-size: contain;
+        min-height: 100vh;
+    }
+
     .user-info {
         font-size: 24px;
+        background-color: rgba(255, 255, 255, 0.85);
+
         h3 {
             padding: 20px 0;
             text-align: center;
             font-size: 28px;
             border-bottom: 1px solid #ececec;
         }
-        .user-info {
+
+        .user-info-box {
             display: flex;
+
             .user-info-avatar {
                 width: 40vw;
                 height: 40vw;
                 box-sizing: border-box;
                 border-right: 1px solid #ececec;
                 border-bottom: 1px solid #ececec;
+
                 img {
                     width: 100%;
                     height: 100%;
@@ -106,12 +176,55 @@
     }
 
     .user-experience {
-        font-size: 24px;
+        font-size: 28px;
+        word-break: break-word;
+        background-color: rgba(255, 255, 255, 0.85);
+
         h3 {
             padding: 20px 0;
             text-align: center;
             font-size: 28px;
             border-bottom: 1px solid #ececec;
+        }
+
+        .user-experience-ul {
+
+            li {
+                padding: 30px 30px 0;
+                border-bottom: 1px solid #ececec;
+            }
+
+            li > div {
+                margin-bottom: 40px;
+            }
+
+            .work-time {
+                display: flex;
+                justify-content: space-between;
+                font-weight: 500;
+            }
+
+            .work-duty {
+                font-weight: 500;
+            }
+
+            .work-description {
+                div {
+                    font-weight: 500;
+                    margin-bottom: 10px;
+                }
+            }
+
+            .project-description {
+                h4 {
+                    margin-bottom: 10px;
+                    font-weight: 500;
+                }
+
+                p {
+                    margin-bottom: 10px;
+                }
+            }
         }
     }
 </style>
